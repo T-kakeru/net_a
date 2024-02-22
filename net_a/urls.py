0a14b8c35c2b855777f8a8c2c873460357337165
T-kakeru <kakeru.takehana0918@gmail.com>
2024-02-22 10:59:31 +0900

    urls.py views.pyを画面遷移図を参照し関数以外を適用

--- net_a/urls.py
+++ net_a/urls.py
@@ -3,8 +3,22 @@ from . import views
 
 app_name = 'net_a'
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('my_page/', views.my_page, name='my_page')
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('sign_in', views.sign_in, name='sign_in'),
+    path('setting', views.setting, name='setting'),
+    path('search', views.search, name='search'),
+    path('privacy', views.privacy, name='privacy'),
+    path('net_a_tutorial', views.net_a_tutorial, name='net_a_tutorial'),
+    path('my_page/', views.my_page, name='my_page'),
+    path('my_fish', views.my_fish, name='my_fish'),
+    path('history', views.history, name='history'),
+    path('fish_info', views.fish_info, name='fish_info'),
+    path('favorite', views.favorite, name='favorite'),
+    path('edit_fish_check', views.edit_fish_check, name='edit_fish_check'),
+    path('edit_fish', views.edit_fish, name='edit_fish'),
+    path('base', views.base, name='base'),
+    path('add_fish_check', views.add_fish_check, name='add_fish_check'),
+    path('add_fish', views.add_fish, name='add_fish')
 ]
 
 """my_pageへのURL設定↑（1行）"""
--- net_a/views.py
+++ net_a/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
-
-"""my_pageへのURL設定"""
+    return render(request, 'sign_up.html')
+def index(request):
+    return render(request, 'sign_in.html')
+def index(request):
+    return render(request, 'setting.html')
+def index(request):
+    return render(request, 'search.html')
+def index(request):
+    return render(request, 'privacy.html')
+def index(request):
+    return render(request, 'net_a_tutorial.html')
 def my_page(request):
     return render(request, 'my_page.html')
+def index(request):
+    return render(request, 'my_fish.html')
+def index(request):
+    return render(request, 'history.html')
+def index(request):
+    return render(request, 'fish_info.html')
+def index(request):
+    return render(request, 'favorite.html')
+def index(request):
+    return render(request, 'edit_fish_check.html')
+def index(request):
+    return render(request, 'base.html')
+def index(request):
+    return render(request, 'add_fish_check.html')
+def index(request):
+    return render(request, 'add_fish.html')
 
+
+"""
+オブジェクト化したい
+for page in pages:
+    def index(request):
+        return render(request, str(page) + '.html')
+        
+
+"""
 
 """
 ビューずどっとぱいがないときに、GPTが教えてくれたやつ
